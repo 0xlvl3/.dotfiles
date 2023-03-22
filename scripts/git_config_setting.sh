033e7dc28ae919d7c549e0dca884f81f6bd97cc1
0xlvl3 <cryptochoccy@gmail.com>
2023-03-22 03:04:32 +0000

    Adding some scripts

---
+++ scripts/git_config_setting.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Git config script"
+
+# Name prompt
+echo -n "Enter your github username: "
+read name
+
+# Email prompt
+echo -n "Enter your github email: "
+read email
+
+# Editor prompt
+echo -n "Enter your editor of choice: "
+read editor
+
+# Set git options
+git config --global user.name "$name"
+git config --global user.email "$email"
+git config --global core.editor "$editor"
+git config --global color.ui true # Colored git output
+git config --global --list 
+
+echo "Git configured :)"
---
+++ scripts/zsh_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
